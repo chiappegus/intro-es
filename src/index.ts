578b0f4933eeb2a5dd1fe9c13dfbdac5ba91575f
chiappegus <chiappegus@gmail.com>
2024-04-28 17:22:48 +0000

    cambiando todo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as algokit from '@algorandfoundation/algokit-utils';
 
 async function main() {
-    console.log('Hola Mundo!')
+  const algorand =algokit.AlgorandClient.defaultLocalNet();
+  console.log('hola todo bien: npm install acordate !!!) ')
+
 }
 
 main();
