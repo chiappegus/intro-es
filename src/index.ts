9c86f7a01136996db59976777872cccf5b92a059
gustavoch <chiappegus@gmial.com>
2024-04-30 01:06:10 -0300

    "completando info"

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,45 @@ import * as algokit from '@algorandfoundation/algokit-utils';
 async function main() {
     console.log('Hola Mundo!')
     const algorand =algokit.AlgorandClient.defaultLocalNet();
-    console.log('hola todo bien: npm install acordate !!!) ')
+    console.log('hola todo bien: npm install acordate  y del algokit localnet !!!) ')
+    console.log('npx tsx ./src/index.ts')
       // ===== Crear dos cuentas =====
       const alice = algorand.account.random()
       const bob = algorand.account.random();
-  
+
       console.log("Alice's Address:", alice.addr);
       console.log("bob's Address:", bob.addr);
   
       // ===== Mostrar información de la cuenta de Alice =====
       console.log("Alice's Account:", await algorand.account.getInformation(alice.addr));
   
-      console.log("bob's Account:", await algorand.account.getInformation(alice.addr));
+      console.log("bob's Account:", await algorand.account.getInformation(bob.addr));
+
+
+      // ahora vamos a darle algos a Alice 
+      // en Algorand por suerte se usa un dispenser de algos.
+
+      const dispenser = await algorand.account.dispenser();
+
+      await algorand.send.payment({
+
+         sender:dispenser.addr,
+         receiver: alice.addr,
+         amount :algokit.algos(10),
+      }
+      )
+
+     // ===== Mostrar información de la cuenta de Alice =====
+     console.log("Alice's Account:", await algorand.account.getInformation(alice.addr));
+     /*
+      ahora vamos a enviarle un asset a bob , primero vamos a crearle el asset a alice
+     */
+
+
+
+    
+     
+
       
 
 }
